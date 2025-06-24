1811be218d76d559c85cdd6490008aa2328d0b11
Bio <razakaalbiok@gmail.com>
2025-06-24 22:47:20 +0700

    done 8

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -9,7 +9,7 @@ export const Hero = () => {
         loop
         className="rotate-180 absolute top-[-340px] md:top-[-340px] sm:top-[-200px] left-0 w-full max-w-full h-full object-cover -z-30 overflow-hidden"
       >
-        <source src="/videos/blackhole.webm" type="video/webm" />
+        <source src="/AlbioPortfolio.github.io/videos/blackhole.webm" type="video/webm" />
       </video>
 
       <HeroContent />
