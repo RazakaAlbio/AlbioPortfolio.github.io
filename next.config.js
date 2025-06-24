9be8ea5c396f7a22c3eb54a22dc02723c8f5f085
Bio <razakaalbiok@gmail.com>
2025-06-24 20:31:21 +0700

    config fix 4

--- next.config.js
+++ next.config.js
@@ -1,16 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'AlbioPortfolio.github.io';
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '', // Kosongkan untuk repository GitHub Pages utama
-  assetPrefix: '', // Kosongkan untuk repository GitHub Pages utama
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}` : '',
   trailingSlash: true,
-  // Tambahkan ini untuk memastikan asset ter-copy
   experimental: {
-   outputFileTracingRoot: process.cwd(),
- },
+    outputFileTracingRoot: process.cwd(),
+  },
 };
 
 module.exports = nextConfig;
