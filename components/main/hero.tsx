9f43670d16a2387f5ad18903818b64813fe918e7
Bio <razakaalbiok@gmail.com>
2025-06-24 23:28:15 +0700

    donegit add .

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -7,9 +7,9 @@ export const Hero = () => {
         autoPlay
         muted
         loop
-        className="rotate-180 absolute top-[-340px] md:top-[-340px] sm:top-[-200px] left-0 w-full max-w-full h-full object-cover -z-10 overflow-hidden"
+        className="rotate-180 fixed inset-0 w-full h-full object-cover -z-30 overflow-hidden"
       >
-        <source src="/AlbioPortfolio.github.io/videos/blackhole.webm" type="video/webm" />
+        <source src={"/AlbioPortfolio.github.io/videos/blackhole.webm"} type="video/webm" />
       </video>
 
       <HeroContent />
