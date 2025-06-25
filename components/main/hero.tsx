48a509ea15888fb5327e51d3ee3593f878b2a817
Bio <razakaalbiok@gmail.com>
2025-06-25 22:31:57 +0700

    black hole fix

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -7,7 +7,7 @@ export const Hero = () => {
         autoPlay
         muted
         loop
-        className="rotate-180 absolute top-[-340px] md:top-[-340px] sm:top-[-200px] left-0 w-full max-w-full h-full object-cover -z-10 overflow-hidden"
+        className="rotate-180 absolute top-[-200px] md:top-[-250px] sm:top-[-150px] left-0 w-full max-w-full h-auto object-contain -z-30 overflow-hidden pointer-events-none scale-110"
       >
         <source src="/AlbioPortfolio.github.io/videos/blackhole.webm" type="video/webm" />
       </video>
